9bf1c6d9b28ea1adbebd7c709bb7b89a9c783ffc
Isacco-B <git@isaccobertoli.com>
2024-02-23 16:05:30 +0100

    create profile model

---
+++ api/models/profile.model.js
@@ -0,0 +1,112 @@
+import mongoose from "mongoose";
+
+const profileSchema = new mongoose.Schema(
+  {
+    // User
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      unique: true,
+    },
+    // Personal Info
+    firstName: {
+      type: String,
+      required: [true, "Your name is required"],
+    },
+    lastName: {
+      type: String,
+      required: [true, "Your last name is required"],
+    },
+    profilePicture: {
+      type: String,
+      default:
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    },
+    phoneNumber: {
+      type: String,
+      max: 12,
+      required: [true, "Your phone number is required"],
+    },
+    dateOfBirth: {
+      type: Date,
+      required: [true, "Your date of birth is required"],
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female", "other"],
+      required: [true, "Your gender is required"],
+    },
+    city: {
+      type: String,
+      required: [true, "Your city is required"],
+    },
+    // Description
+    description: {
+      type: String,
+    },
+    // Work Info
+    isWorking: {
+      type: Boolean,
+      default: false,
+    },
+    lookingForJob: {
+      type: Boolean,
+      default: false,
+    },
+    remoteWork: {
+      type: Boolean,
+      default: false,
+    },
+    availability: {
+      type: String,
+      enum: ["full time", "part time"],
+      default: "full time",
+    },
+    // Education Info
+    education: {
+      type: String,
+      enum: ["high school", "bachelor", "master", "doctorate"],
+      default: "high school",
+    },
+    graduationDate: {
+      type: Date,
+    },
+    // Social Info
+    instagram: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    linkedin: {
+      type: String,
+    },
+    twitter: {
+      type: String,
+    },
+    github: {
+      type: String,
+    },
+    website: {
+      type: String,
+    },
+    // Skills
+    hardSkills: {
+      type: [String],
+      default: [],
+    },
+    softSkills: {
+      type: [String],
+      default: [],
+    },
+    // Certifications
+    certifications: {
+      type: [String],
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+const Profile = mongoose.model("Profile", profileSchema);
+export default Profile;
