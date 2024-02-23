696a4ec3d00d1143a7da900345b35c792051a24b
Isacco-B <git@isaccobertoli.com>
2024-02-23 16:07:21 +0100

    create master model

---
+++ api/models/master.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const masterSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Master title is required"],
+  },
+  description: {
+    type: String,
+    required: [true, "Master description is required"],
+  },
+  courses: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+      required: [true, "Master course is required"],
+    },
+  ],
+  durarion: {
+    type: Number,
+    required: [true, "Master duration is required"],
+  },
+},{datatimes: true});
+
+const Master = mongoose.model("Master", masterSchema);
+export default Master;
