b6b5b80c8896ddfce32018f7fe3649d1154a1cbc
Isacco-B <git@isaccobertoli.com>
2024-03-19 18:38:49 +0100

    first commit

---
+++ api/controllers/profile.controller.js
@@ -0,0 +1,10 @@
+import Profile from "../models/profile.model.js";
+import { errorHandler } from "../middleware/errorHandler.js";
+
+export const getProfiles = async (req, res, next) => {};
+
+export const createProfile = async (req, res, next) => {};
+
+export const updateProfile = async (req, res, next) => {};
+
+export const deleteProfile = async (req, res, next) => {};
