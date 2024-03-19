d2d80e2ae0acb76cef54e7a2ec1bf0ce11ec52ac
Isacco-B <git@isaccobertoli.com>
2024-03-19 18:43:14 +0100

    route update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "./context/ThemeProvider";
+import { ROLES } from "./constants";
 
-function App() {
-  const [count, setCount] = useState(0)
+// Components
+import Layouts from "./components/Layout";
+import Public from "./components/Public";
+import DashLayout from "./components/dashboard/DashLayout";
+import ScrollToTop from "./components/ScrollToTop";
 
+// Pages
+import About from "./pages/About";
+
+// Features
+import Welcome from "./features/auth/components/Welcome";
+import SignIn from "./features/auth/components/SignIn";
+import SignUp from "./features/auth/components/SignUp";
+import MasterList from "./features/master/components/MasterList";
+import Prefetch from "./features/auth/components/Prefetch";
+import PersistLogin from "./features/auth/components/PersistLogin";
+import RequireAuth from "./features/auth/components/RequireAuth";
+import ProfileDetails from "./features/profile/components/ProfileDetails";
+import LeaderboardList from "./features/leaderboard/components/LeaderboardList";
+import AccountDetails from "./features/account/components/AccountDetails";
+import ProfilleUpdate from "./features/profile/components/ProfilleUpdate";
+
+
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Layouts />}>
+          {/* Public Routes */}
+          <Route index element={<Public />} />
+          <Route path="login" element={<SignIn />} />
+          <Route path="register" element={<SignUp />} />
+          <Route path="about" element={<About />} />
+          {/* Protected Routes */}
+          <Route element={<PersistLogin />}>
+            <Route
+              element={<RequireAuth allowedRoles={[...Object.values(ROLES)]} />}
+            >
+              <Route element={<Prefetch />}>
+                <Route path="dash" element={<DashLayout />}>
+                  <Route index element={<Welcome />} />
+                  <Route
+                    element={<RequireAuth allowedRoles={[ROLES.Student]} />}
+                  >
+                    <Route path="master" element={<MasterList />} />
+                  </Route>
+                  <Route path="corsi" element={<Welcome />} />
+                  <Route path="classifica" element={<LeaderboardList />} />
+                  <Route path="account" element={<AccountDetails />} />
+                  <Route path="profile" element={<ProfileDetails />} />
+                  <Route path="profile/edit" element={<ProfilleUpdate />} />
+                </Route>
+              </Route>
+            </Route>
+          </Route>
+        </Route>
+      </Routes>
+    </ThemeProvider>
+  );
 }
-
-export default App
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+import { store } from "./app/store.js";
+import { Provider } from "react-redux";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
