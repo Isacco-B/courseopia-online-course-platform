0b5166c0db30f7c1ae36429b7bb8ebafaf3976e2
Isacco-B <git@isaccobertoli.com>
2024-04-26 19:44:01 +0200

    general update

--- api/index.js
+++ api/index.js
@@ -1,62 +1,53 @@
 import dotenv from "dotenv";
 import express from "express";
-import multer from "multer";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import corsOptions from "./config/corsOptions.js";
-import connectDB from "./config/dbConn.js";
 import mongoose from "mongoose";
 import { logEvents, logger } from "./middleware/logger.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "profile_images/");
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, file.fieldname + "-" + uniqueSuffix);
-  },
-  limits: { fileSize: 1 },
-});
-const upload = multer({ storage: storage });
-
-const PORT = process.env.PORT || 3000;
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = process.env.PORT || 3000;
+
+// Routes
 import userRoutes from "./routes/user.routes.js";
 import authRoutes from "./routes/auth.routes.js";
 import lessonRoutes from "./routes/lesson.routes.js";
 import courseRoutes from "./routes/course.routes.js";
-import masterRoutes from "./routes/master.routes.js"
-import profileRoutes from "./routes/profile.routes.js"
-
-connectDB();
+import masterRoutes from "./routes/master.routes.js";
+import profileRoutes from "./routes/profile.routes.js";
+import categoryRoutes from "./routes/category.routes.js";
+import projectRoutes from "./routes/project.routes.js";
 
 function startServer() {
   const app = express();
 
-  app.use(express.static("public"));
-
+  // Middleware
   app.use(logger);
   app.use(cors(corsOptions));
   app.use(express.json());
   app.use(cookieParser());
 
-  app.get("/", (req, res) => {
-    res.json("Hello World");
-  });
+  // Static Files
+  app.use(express.static(path.join(__dirname, "assets")));
 
-  app.post("/api/upload", upload.single("avatar"), (req, res) => {
-    res.json(req.file);
-  });
-
+  // Routes
   app.use("/api/users", userRoutes);
   app.use("/api/auth", authRoutes);
   app.use("/api/lessons", lessonRoutes);
   app.use("/api/courses", courseRoutes);
   app.use("/api/masters", masterRoutes);
-  app.use("/api/profile", profileRoutes);
+  app.use("/api/categories", categoryRoutes);
+  app.use("/api/profiles", profileRoutes);
+  app.use("/api/projects", projectRoutes);
 
+  // 404
   app.all("*", (req, res) => {
     res.status(404);
     if (req.accepts("json")) {
@@ -66,6 +57,7 @@ function startServer() {
     }
   });
 
+  // Error Handling
   app.use((err, req, res, next) => {
     logEvents(
       `${err.name}: ${err.message}\t${req.method}\t${req.url}\t${req.headers.origin}`,
@@ -78,23 +70,22 @@ function startServer() {
       .json({ success: false, statusCode, message, isError: true });
   });
 
-  app.listen(3000, () =>
-    console.log(
-      "Listening on http://localhost:3000/" +
-        "\nApiDoc: http://localhost:3000/api-docs/"
-    )
-  );
+  app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}/`));
 }
 
-mongoose.connection.once("open", () => {
-  console.log("Connected to MongoDB");
-  startServer();
-});
-
-mongoose.connection.on("error", (err) => {
-  console.log(err);
-  logEvents(
-    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
-    "mongoErrLog.log"
-  );
-});
+// Connect to MongoDB
+console.log(`Connecting to ${process.env.DB_HOST}`);
+mongoose
+  .connect(process.env.DB_HOST)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    startServer();
+  })
+  .catch((err) => {
+    console.log("Could not connect to MongoDB");
+    console.log(err);
+    logEvents(
+      `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+      "mongoErrLog.log"
+    );
+  });
