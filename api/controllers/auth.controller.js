eccffa5cf3fe06a57142f8641a8209c42c2204d6
Isacco-B <git@isaccobertoli.com>
2024-04-26 14:47:45 +0200

    Complete auth controller functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,13 +1,16 @@
 import User from "../models/user.model.js";
+import AccountVerification from "../models/account-verification.js";
 import bcrypjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { errorHandler } from "../middleware/errorHandler.js";
 import { validationResult } from "express-validator";
+import { generateRandomNumber } from "../utils/randomNumber.js";
+import { accountVerificationEmail } from "../utils/emailTemplates/accountVerificationEmail.js";
 
 export const signUp = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return next(errorHandler(400, "Invalid Credentials"));
+    return res.status(400).json({ errors: errors.array() });
   }
   const { firstName, lastName, email, password, confirmPassword } = req.body;
 
@@ -44,51 +47,44 @@ export const signIn = async (req, res, next) => {
   const { email, password } = req.body;
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return next(errorHandler(400, "Invalid Credentials"));
+    return res.status(400).json({ errors: errors.array() });
   }
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) {
-      return next(errorHandler(404, "Invalid Credentials"));
+      return next(errorHandler(404, "User not found"));
     }
-    const validPassword = bcrypjs.compareSync(password, validUser.password);
+    const validPassword = await bcrypjs.compare(password, validUser.password);
     if (!validPassword) {
       return next(errorHandler(400, "Invalid Credentials"));
     }
 
-    const match = await bcrypjs.compare(password, validUser.password);
-    if (!match) {
-      return next(errorHandler(400, "Invalid Credentials"));
-    }
-
-    // Generate JWT
     const accessToken = jwt.sign(
       {
         UserInfo: {
-          firstName: validUser.firstName,
+          id: validUser._id,
+          slug: validUser.slug,
           email: validUser.email,
           role: validUser.role,
         },
       },
       process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: "15m" } // Change to 15m in production
+      { expiresIn: "15m" }
     );
 
-    // Generate Refresh Token
     const refreshToken = jwt.sign(
-      { email: validUser.email },
+      { id: validUser._id },
       process.env.REFRESH_TOKEN_SECRET,
-      { expiresIn: "7d" } // Change to 7d in production
+      { expiresIn: "7d" }
     );
 
     res.cookie("jwt", refreshToken, {
-      httpOnly: true, // Accessible only by web server
-      secure: true, // Required only when using HTTPS
-      sameSite: "None", // Required only if site is using cookie on different domain
-      maxAge: 7 * 24 * 60 * 60 * 1000, // Change to 7d in production
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
-    // Send accessToken in response
     res.json({ accessToken });
   } catch (error) {
     next(error);
@@ -98,7 +94,7 @@ export const signIn = async (req, res, next) => {
 export const signOut = async (req, res) => {
   const cookies = req.cookies;
   if (!cookies?.jwt) {
-    return res.sendStatus(204); // No content
+    return res.status(400).json("You are already logged out");
   }
   res.clearCookie("jwt", { httpOnly: true, sameSite: "None", secure: true });
   res.json({ message: "Cookie cleared" });
@@ -109,25 +105,121 @@ export const refresh = async (req, res, next) => {
 
   if (!cookies?.jwt) return next(errorHandler(401, "Unathorized"));
   const refreshToken = cookies.jwt;
-
-  jwt.verify(
-    refreshToken,
-    process.env.REFRESH_TOKEN_SECRET,
-    async (err, decoded) => {
-      if (err) return next(errorHandler(403, "Forbidden"));
-      const user = await User.findOne({ email: decoded.email });
-      const accessToken = jwt.sign(
-        {
-          UserInfo: {
-            firstName: user.firstName,
-            email: user.email,
-            role: user.role,
+  try {
+    jwt.verify(
+      refreshToken,
+      process.env.REFRESH_TOKEN_SECRET,
+      async (err, decoded) => {
+        if (err) return next(errorHandler(403, "Forbidden"));
+        const user = await User.findOne({ _id: decoded.id });
+        if (!user) return next(errorHandler(404, "User not found"));
+        const accessToken = jwt.sign(
+          {
+            UserInfo: {
+              id: user._id,
+              slug: user.slug,
+              email: user.email,
+              role: user.role,
+            },
           },
-        },
-        process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn: "15m" }
-      );
-      res.json({ accessToken });
+          process.env.ACCESS_TOKEN_SECRET,
+          { expiresIn: "15m" }
+        );
+        res.json({ accessToken });
+      }
+    );
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const changePassword = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { id } = req.user;
+  const { password, confirmPassword } = req.body;
+  if (password !== confirmPassword) {
+    return next(errorHandler(400, "Password do not match"));
+  }
+  const passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+  if (!passwordRegex.test(password)) {
+    return next(
+      errorHandler(
+        400,
+        "Your password must be between 8 and 24 characters long, and contain at least one lowercase letter, one uppercase letter, one number, and one special character (!@#$%)."
+      )
+    );
+  }
+  const hashedPassword = bcrypjs.hashSync(password, 10);
+  try {
+    const user = await User.findByIdAndUpdate(
+      id,
+      { password: hashedPassword },
+      { new: true }
+    );
+    res.json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const createVerifyAccountToken = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  try {
+    const { id, email } = req.user;
+    const user = await User.findById(id);
+    if (!user) {
+      return next(errorHandler(404, "User not found"));
     }
-  );
+
+    const OTP = generateRandomNumber(6);
+    const hashedOTP = bcrypjs.hashSync(OTP, 10);
+    await AccountVerification.updateOne(
+      { user: user._id },
+      { token: hashedOTP },
+      { upsert: true }
+    );
+    await accountVerificationEmail(email, OTP);
+    res.status(200).json({ message: "Verification code sent" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const checkVerifyAccountToken = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const { token } = req.body;
+  const { id } = req.user;
+  try {
+    const accountVerification = await AccountVerification.findOne({
+      user: id,
+    });
+    if (!accountVerification) {
+      return next(errorHandler(404, "Account verification not found"));
+    }
+    const validToken = await bcrypjs.compare(token, accountVerification.token);
+    if (!validToken) {
+      return next(errorHandler(400, "Invalid token"));
+    }
+    const user = await User.findOne({ _id: accountVerification.user });
+    if (!user) {
+      return next(errorHandler(404, "User not found"));
+    }
+    user.verified = true;
+    await user.save();
+    await accountVerification.deleteOne({ _id: accountVerification._id });
+    res.status(200).json({ message: "Account verified successfully" });
+  } catch (error) {
+    next(error);
+  }
 };
