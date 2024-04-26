60f6f1f60e7b2e290d9f749135e425a5dc1e1958
Isacco-B <git@isaccobertoli.com>
2024-04-26 12:04:56 +0200

    Complete Lesson Model functionality

--- api/models/lesson.model.js
+++ api/models/lesson.model.js
@@ -1,23 +1,41 @@
 import mongoose from "mongoose";
+import slugify from "slugify";
 
-const LessonSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "Lesson title is required"],
+const LessonSchema = new mongoose.Schema(
+  {
+    slug: {
+      type: String,
+      unique: true,
+    },
+    title: {
+      type: String,
+      required: [true, "Lesson title is required"],
+    },
+    content: {
+      type: String,
+      required: [true, "Lesson content is required"],
+    },
+    duration: {
+      type: Number,
+      required: [true, "Lesson duration is required"],
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "LessonCategory",
+      required: [true, "Lesson category is required"],
+    },
   },
-  content: {
-    type: String,
-    required: [true, "Lesson content is required"],
-  },
-  duration: {
-    type: Number,
-    required: [true, "Lesson duration is required"],
-  },
-  completed: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { timestamps: true }
+);
+
+LessonSchema.pre("save", async function (next) {
+  try {
+    this.slug = slugify(this.title + " " + new Date(this.createdAt).toISOString(), { lower: true });
+    next();
+  } catch (error) {
+    next(error);
+  }
+})
 
 const Lesson = mongoose.model("Lesson", LessonSchema);
 export default Lesson;
