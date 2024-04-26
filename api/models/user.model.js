4fe3160324337cbdec2c28ad1ebf29970fba4342
Isacco-B <git@isaccobertoli.com>
2024-04-26 12:04:23 +0200

    Complete User Model functionality

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,7 +1,17 @@
 import mongoose from "mongoose";
+import Profile from "./profile.model.js";
+import slugify from "slugify";
 
 const userSchema = new mongoose.Schema(
   {
+    slug: {
+      type: String,
+      unique: true,
+    },
+    profile: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Profile",
+    },
     firstName: {
       type: String,
       required: [true, "Your name is required"],
@@ -24,13 +34,50 @@ const userSchema = new mongoose.Schema(
       enum: ["student", "teacher", "admin"],
       default: "student",
     },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
     active: {
       type: Boolean,
       default: true,
     },
+    totalPoints: {
+      type: Number,
+      default: 0,
+    },
+    currentMaster: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Master",
+    },
+    coursesCompleted: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+      },
+    ],
+    lessonsCompleted: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Lesson",
+      },
+    ],
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  try {
+    if (!this.isNew) {
+      return next();
+    }
+    this.slug = slugify(`${this.firstName}-${this.lastName}`, { lower: true });
+    const profile = await Profile.create({ user: this._id });
+    this.profile = profile._id;
+  } catch (error) {
+    next(error);
+  }
+});
 
 const User = mongoose.model("User", userSchema);
 export default User;
