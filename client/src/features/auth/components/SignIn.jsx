2b0aeb8edc928ce9d60ece04cdffaa33f172449b
Isacco-B <git@isaccobertoli.com>
2024-09-27 15:38:31 +0200

    add default login credentials

--- client/src/features/auth/components/SignIn.jsx
+++ client/src/features/auth/components/SignIn.jsx
@@ -12,6 +12,7 @@ import FormInput from "@/components/FormInput";
 import { Eye } from "lucide-react";
 import { ThreeDots } from "react-loader-spinner";
 import { useTitle } from "@/hooks/useTitle";
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
 
 export default function SignIn({forgotPasswordText, signUpText}) {
   useTitle("Login | Courseopia");
@@ -56,72 +57,92 @@ export default function SignIn({forgotPasswordText, signUpText}) {
   const canSave = formData.email && formData.password;
 
   return (
-    <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-      <FormInput
-        label="Email*"
-        id="email"
-        placeholder="Inserisci Email"
-        value={formData.email}
-        onChange={handleChange}
-        type="email"
-        autoComplete={false}
-        required={true}
-      />
-      <div className="grid w-full items-center gap-1.5 relative">
-        <Label htmlFor="password">Password*</Label>
-        <div className="flex items-center gap-1.5">
-          <Input
-            name="password"
-            type={showPwd ? "text" : "password"}
-            id="password"
-            placeholder="Inserisci Password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-          <span className="absolute right-3 cursor-pointer">
-            <Eye
-              size={22}
-              onClick={() => setShowPwd(!showPwd)}
-              className={`${showPwd && "text-primary"} hover:scale-[1.2]`}
-            />
-          </span>
-        </div>
-      </div>
-      {forgotPasswordText && (
-        <Link
-          to="/password-dimenticata"
-          className="text-primary opacity-80 hover:opacity-100"
-        >
-          {forgotPasswordText}
-        </Link>
-      )}
-
-      <Button
-        className="uppercase font-semibold w-1/3 mx-auto"
-        type="submit"
-        disabled={!canSave || isLoading}
-      >
-        Continua
-      </Button>
-      {signUpText && (
-        <Link
-          to="/register"
-          className="text-primary opacity-80 hover:opacity-100 text-center"
-        >
-          Non hai un account? REGISTRATI ORA
-        </Link>
-      )}
-
-      <div className="mx-auto">
-        <ThreeDots
-          visible={isLoading}
-          height="40"
-          width="40"
-          color="#22c55e"
-          ariaLabel="three-dots-loading"
+    <>
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <FormInput
+          label="Email*"
+          id="email"
+          placeholder="Inserisci Email"
+          value={formData.email}
+          onChange={handleChange}
+          type="email"
+          autoComplete={false}
+          required={true}
         />
-      </div>
-    </form>
+        <div className="grid w-full items-center gap-1.5 relative">
+          <Label htmlFor="password">Password*</Label>
+          <div className="flex items-center gap-1.5">
+            <Input
+              name="password"
+              type={showPwd ? "text" : "password"}
+              id="password"
+              placeholder="Inserisci Password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+            <span className="absolute right-3 cursor-pointer">
+              <Eye
+                size={22}
+                onClick={() => setShowPwd(!showPwd)}
+                className={`${showPwd && "text-primary"} hover:scale-[1.2]`}
+              />
+            </span>
+          </div>
+        </div>
+        {forgotPasswordText && (
+          <Link
+            to="/password-dimenticata"
+            className="text-primary opacity-80 hover:opacity-100"
+          >
+            {forgotPasswordText}
+          </Link>
+        )}
+
+        <Button
+          className="uppercase font-semibold w-1/3 mx-auto"
+          type="submit"
+          disabled={!canSave || isLoading}
+        >
+          Continua
+        </Button>
+        {signUpText && (
+          <Link
+            to="/register"
+            className="text-primary opacity-80 hover:opacity-100 text-center"
+          >
+            Non hai un account? REGISTRATI ORA
+          </Link>
+        )}
+
+        <div className="mx-auto">
+          <ThreeDots
+            visible={isLoading}
+            height="40"
+            width="40"
+            color="#22c55e"
+            ariaLabel="three-dots-loading"
+          />
+        </div>
+      </form>
+      <Card>
+        <CardTitle className="text-center text-lg p-4">
+          You can create a new account or login with this credentials.
+        </CardTitle>
+        <CardContent className="mt-4">
+          <div className="space-y-2 text-sm md:text-lg">
+            <p>
+              <strong>Admin:</strong> admin@gmail.com - Admin123!
+            </p>
+            <p>
+              <strong>Teacher:</strong> teacher@gmail.com - Teacher123!
+            </p>
+            <p>
+              <strong>Student:</strong> student@gmail.com - Student123!
+            </p>
+          </div>
+        </CardContent>
+      </Card>
+    </>
   );
 }
