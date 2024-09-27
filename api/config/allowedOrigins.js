96a17f2a6f210137181caf00c32b798f24dab057
Isacco-B <git@isaccobertoli.com>
2024-09-27 15:34:50 +0200

    removed

--- api/config/allowedOrigins.js
+++
@@ -1,8 +0,0 @@
-const allowedOrigins = [
-  "http://localhost:3000",
-  "http://localhost:5173",
-  "https://demo7.isaccobertoli.com",
-  "https://demo8.isaccobertoli.com"
-];
-
-export default allowedOrigins;
